26d9a74f705235e982200e8f555a8685292a5f4d
Alexander Fominykh <fominykh.a1809@gmail.com>
2021-11-02 15:27:40 +0300

    + смена пароля с проверкой

--- src/controllers/api-users.controller.js
+++ src/controllers/api-users.controller.js
@@ -2,12 +2,14 @@ const { Router } = require("express");
 const { asyncHandler, requireToken } = require("../middlewares/middlewares");
 const Token = require("../dataBase/models/Token.model");
 const User = require("../dataBase/models/User.model");
+const ErrorResponse = require("../classes/error-response");
 
 const router = Router();
 
 function initRoutes() {
   router.get("/me", asyncHandler(requireToken), asyncHandler(receiveUserInfo));
   router.patch("/me", asyncHandler(requireToken), asyncHandler(updateUserInfo));
+  router.patch("/me/password", asyncHandler(requireToken), asyncHandler(updateUserPassword));
   router.post("/logout", asyncHandler(requireToken), asyncHandler(logoutUser));
 }
 
@@ -17,7 +19,21 @@ async function receiveUserInfo(req, res, _next) {
 }
 
 async function updateUserInfo(req, res, _next) {
+  let user = await User.findByPk(req.userId)
+  req.headers.password = user.password
   let user = await User.update(req.headers, {
+    where: {
+      id: req.userId
+    },
+    returning: true,
+  });
+  res.status(200).json(user);
+}
+
+async function updateUserPassword(req, res, _next) {
+  let user = await User.findByPk(req.userId)
+  if(oldPassword != user.password) throw new ErrorResponse("Wrong password",403)
+  let user = await User.update({password: password}, {
     where: {
       id: req.userId
     },
